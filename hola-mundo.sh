60d1a9da01856e4dec4dec1d8c48a97bf5739510
Miguel1613 <55897396+Miguel1613@users.noreply.github.com>
2023-08-23 16:00:36 -0500

    Create hola-mundo.sh

---
+++ hola-mundo.sh
@@ -0,0 +1,3 @@
+ #!/bin/bash
+  2 # Programa para realizar operaciones utilitarias de Postgres
+  3 echo "Hola bienvenido al curso de programación Bash"                                                         
