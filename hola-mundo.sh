711517b812a2db4b29358c93c943a3796117acc4
Miguel Sarmiento <miguel.sarmiento@hpe.com>
2023-08-23 16:19:18 -0500

    Delete hola-mundo.sh

--- hola-mundo.sh
+++
@@ -1,3 +0,0 @@
- #!/bin/bash
-  # Programa para realizar operaciones utilitarias de Postgres
-  echo "Hola bienvenido al curso de programación Bash"                                                         
